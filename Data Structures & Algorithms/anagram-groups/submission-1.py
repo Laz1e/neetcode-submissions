c8eb647d9f3225cd10dd1d0bfe9c66874139d3ed
Laz1e <83717294+Laz1e@users.noreply.github.com>
2026-06-14 22:59:26 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        res = {}
+
+        for word in strs:
+            asci = [0] * 26
+            arr = list(word)
+
+            for letter in arr:
+                asci[ord(letter) - 97] += 1
+
+            key = ",".join(map(str, asci))
+            print(asci)
+            res.setdefault(key, []).append(word)
+        
+        return list(res.values())
+
+        
